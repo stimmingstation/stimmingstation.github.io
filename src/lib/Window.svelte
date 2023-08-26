51eb8be74c5aaaa950b381fad57329a69946a249
oopsmishap <oopsmishap@proton.me>
2023-08-26 19:32:15 +0100

    Update:
    * Updated logic for video resizing, works somewhat better now
    * Updated dragging logic
    * Added resizing to windows
    * Exported position and size of windows for initial creation
    * Allow windows to not be draggable/re-sizable
    * Added new videos to initial layout

--- src/lib/Window.svelte
+++ src/lib/Window.svelte
@@ -1,102 +1,202 @@
 <script>
     export let title;
 
-    export let posX = 0;
-    export let posY = 0;
+    import {draggable} from "svelte-agnostic-draggable";
+    import {onMount} from "svelte";
+    import {zIndex} from "./store.js";
 
-    let prevPosX = 0;
-    let prevPosY = 0;
+    export let PosX = 0;
+    export let PosY = 0;
 
-    let active = false;
+    export let Width = 500;
+    export let Height = 500;
 
-    let windowNode;
-    let windowChildNode;
+    export let Draggable = true;
+    export let Resizable = true;
 
-    const dragStart = e => {
-        e.preventDefault();
-        active = true;
-    };
 
-    const dragEnd = e => {
-        e.preventDefault();
-        active = false;
-    };
+    let Resizing = false;
+    let InitialWidth = Width;
+    let InitialHeight = Height;
 
-    const drag = e => {
-        e.preventDefault();
+    let Window;
+    let zIndexValue;
 
-        if (active) {
-            posX += e.movementX;
-            posY += e.movementY;
+    // subscribe to the zIndex store
+    zIndex.subscribe((value) => {
+        zIndexValue = value;
+    });
 
-            console.log(`posX: ${posX}, posY: ${posY}`)
+    onMount(() => {
+        // set the initial position of the window
+        Window.style.left = PosX + 'px';
+        Window.style.top = PosY + 'px';
+
+        // add listener to bring window to front
+        Window.addEventListener('mousedown', () => {
+            // increment the zIndex store
+            zIndex.update(n => n + 1);
+            Window.style.zIndex = zIndexValue;
+        });
+
+        // add listener to bring window to front
+        Window.addEventListener('touchstart', () => {
+            // increment the zIndex store
+            zIndex.update(n => n + 1);
+            Window.style.zIndex = zIndexValue;
+        });
+    });
+
+    // map all touch events to mouse events
+    import mapTouchToMouseFor from "svelte-touch-to-mouse";
+
+    mapTouchToMouseFor('.draggable');
+
+    // Svelte Event Handling
+    function onDragMove(Event) {
+        if (Resizable) {
+            if (Resizing) {
+                Width = InitialWidth + Event.detail.position.left - PosX;
+                Height = InitialHeight + Event.detail.position.top - PosY;
+            } else {
+                PosX = Event.detail.position.left;
+                InitialWidth = Width;
+                PosY = Event.detail.position.top;
+                InitialHeight = Height;
+                Resizing = true;
+            }
         }
-    };
-</script>
-
-<div class="workspace-item workspace-item--window"
-     style="transform: translate3d({posX}px,{posY}px, 0);">
-  <div class="workspace-item__header"
-       on:mousedown={dragStart}
-       on:touchstart={dragStart}>
-    <p>{title}</p>
-    <div class="workspace-item__button">
-      <button class="button-window">?</button>
-      <button class="button-window">_</button>
-      <button class="button-window">X</button>
-    </div>
-  </div>
-  <div class="workspace-item__content">
-    <slot/>
-  </div>
-</div>
-
-<svelte:window
-    on:touchend={dragEnd}
-    on:touchmove={drag}
-    on:mouseup={dragEnd}
-    on:mousemove={drag}
-/>
-
-<style>
-    .workspace-item {
-        user-select: none;
     }
 
-    .workspace-item__header {
+    function onDragStop() {
+        Resizing = false;
+    }
+</script>
+
+{#if Draggable}
+
+  <div class="window"
+       use:draggable={{handle:'.window--header', cursor:'grabbing'}}
+       style="width:{Width}px; height:{Height}px;"
+       bind:this={Window}
+  >
+    <div class="draggable window--header">
+      <p class="window--title">{title}</p>
+      <div class="window--button-group">
+        <button class="window--button">?</button>
+        <button class="window--button">_</button>
+        <button class="window--button">X</button>
+      </div>
+    </div>
+    <div class="window--content">
+      <slot/>
+    </div>
+
+    {#if Resizable}
+
+      <div class="draggable window--resize"
+           use:draggable={{helper:'clone', revert:true}}
+           on:drag:move={onDragMove} on:drag:stop={onDragStop}
+      ></div>
+
+    {/if}
+
+  </div>
+
+{:else}
+
+  <div class="window" style="width:{Width}px; height:{Height}px;" bind:this={Window}>
+    <div class="window--header">
+      <p class="window--title">{title}</p>
+      <div class="window--button-group">
+        <button class="window--button">?</button>
+        <button class="window--button">_</button>
+        <button class="window--button">X</button>
+      </div>
+    </div>
+    <div class="window--content">
+      <slot/>
+    </div>
+    {#if Resizable}
+      <div class="draggable window--resize"
+           use:draggable={{helper:'clone', revert:true}}
+           on:drag:move={onDragMove} on:drag:stop={onDragStop}
+      ></div>
+    {/if}
+  </div>
+
+{/if}
+
+<style>
+    .draggable {
+        -webkit-touch-callout: none;
+        -ms-touch-action: none;
+        touch-action: none;
+        -moz-user-select: none;
+        -webkit-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+        cursor: move;
+    }
+
+    .window {
+        position: absolute;
+        left: 0;
+        top: 0;
+        padding: 2px;
+        background: #ccc;
+        border-right: 1px solid gray;
+        border-bottom: 1px solid gray;
+        border-top: 1px solid silver;
+        border-left: 1px solid silver;
+        box-shadow: 1px 0 #000, 0 1px #000, 1px 1px #000, inset 1px 1px #fff, 0 0 150px 1px rgba(0, 0, 0, 0.5);
+        overflow: hidden;
+    }
+
+    .window--header {
         color: #fff;
         margin-bottom: 2px;
         display: flex;
         justify-content: space-between;
-        cursor: move;
         background-color: #1b4eaa;
         font-size: 8px;
         padding: 2px 2px 1px;
         user-select: none;
     }
 
-    .workspace-item--window {
-        position: absolute;
-        left: 0;
-        top: 0;
-        padding: 2px;
-        border-right: 1px solid gray;
-        border-bottom: 1px solid gray;
-        background-color: silver;
-        border: 1px solid #555;
-        background: #ccc;
-        border-top: 1px solid silver;
-        border-left: 1px solid silver;
-        box-shadow: 1px 0 #000, 0 1px #000, 1px 1px #000, inset 1px 1px #fff,
-        0 0 150px 1px rgba(0, 0, 0, 0.5);
+    .window--title {
+        position: relative;
+        overflow: hidden;
+        white-space: nowrap;
     }
 
-    .workspace-item__content {
+    .window--resize {
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        width: 32px;
+        height: 32px;
+        background-color: transparent;
+        background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAiklEQVRYR+WUwQ3AIAwDm3UzUNZtxQ8hhBpIbGhZIKezsVzkJ4z7ZnaXu6oq/wSorVMMwAHqzNvOQQzQAUY/DWIADjBSXmDSd4AO4FnXb3TAozxlB+gAnsxTDMABVpSH7AAdYEX5mR2IVD5lgA4QmfmUAThApvJXO0AHyFS+ZweQyrsG6ADIzNtbD4OSoCHdTWtaAAAAAElFTkSuQmCC");
+        cursor: nwse-resize;
+    }
+
+    .window--content {
         -webkit-user-drag: none;
         position: relative;
+        padding: 3px;
+        width: 100%;
+        height: 100%;
     }
 
-    .button-window {
+    .window--button-group {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .window--button {
         color: #000;
         border-top: 1px solid #fff;
         border-left: 1px solid #fff;
